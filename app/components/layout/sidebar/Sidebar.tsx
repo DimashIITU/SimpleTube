ee4a5415d4cd5f13f7aa1e02c1c0e7a85a3e4403
AndrNikitin <tteezzaassdd@gmail.com>
2023-03-07 23:56:27 +0600

    res

---
+++ app/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import { FC } from 'react'
+
+import Menu from '@/components/layout/sidebar/menu/Menu'
+import { menu } from '@/components/layout/sidebar/menu/menu.data'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import { api } from '@/store/api/api'
+
+import styles from './Sidebar.module.scss'
+
+const Sidebar: FC = () => {
+	const { user } = useAuth()
+
+	const { data } = api.useGetProfileQuery(null, {
+		skip: !user
+	})
+
+	return (
+		<aside className={styles.sidebar}>
+			<Link href='/'>
+				<a className={styles.logo}>SimpleTube</a>
+			</Link>
+
+			<Menu title='Меню' items={menu} />
+
+			{user && (
+				<Menu
+					title='Мои подписки'
+					items={
+						data?.subscriptions.map(({ toChannel }) => ({
+							image: toChannel.avatarPath,
+							title: toChannel.name,
+							link: '/c/' + toChannel.id
+						})) || []
+					}
+				/>
+			)}
+		</aside>
+	)
+}
+
+export default Sidebar
